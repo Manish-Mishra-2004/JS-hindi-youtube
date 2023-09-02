ba280778ecff5f64fb2580831529c00fd64470f0
Manish-Mishra-2004 <143214443+Manish-Mishra-2004@users.noreply.github.com>
2023-09-02 19:03:59 +0000

    day 1

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+ const accountId = 144553
+ let accountEmail = "Manish@gmail.com"
+ var accountPassword = "12345"
+ accountCity = "Jaipur"
+let accountState;
+ console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,13 @@
+"use strict"; //treat all js code as newer version
+// alert (3+3) // we are using node js, not browser
+// tc39 documentation
+
+let name = "Manish" // string
+let age = 18 // number   bigint
+let isLoggedIn = false //boolean
+// null => standalone value
+// undefined
+// symbol => unique
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
